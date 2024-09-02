0b5eab74dc44a27ccc50ae92d3b42e00f81c4f6e
shch989 <tankpop11@gmail.com>
2024-09-02 16:59:30 +0900

    간단 요청 설계

---
+++ src/app.controller.ts
@@ -0,0 +1,62 @@
+import { Controller, Delete, Get, NotFoundException, Param, Patch, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+interface Movie {
+  id: number
+  title: string
+}
+
+@Controller("movie")
+export class AppController {
+  private movies: Movie[] = [
+    {
+      id: 1,
+      title: "해리포터"
+    },
+    {
+      id: 2,
+      title: "반지의 제왕"
+    }
+  ];
+
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getMovies() {
+    return this.movies
+  }
+
+  @Get("/:id")
+  getMovie(@Param("id") id: string) {
+    const movie = this.movies.find((m) => m.id === +id)
+
+    if(!movie) {
+      throw new NotFoundException("존재하지 않는 ID의 영화입니다!")
+    }
+
+    return movie
+  }
+
+  @Post()
+  postMovie() {
+    return {
+      id: 3,
+      name: '어벤저스',
+      character: ["아이언맨", "캡틴아메리카"]
+    }
+  }
+
+  @Patch("/:id")
+  patchMovie() {
+    return {
+      id: 3,
+      name: '어벤져스',
+      character: ['아이언맨', '블랙위도우']
+    }
+  }
+
+  @Delete("/:id")
+  deleteMovie() {
+    return 3
+  }
+}
